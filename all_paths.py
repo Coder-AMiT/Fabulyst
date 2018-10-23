e68012211a74474b83bef29c45170ddc7254c2e4
AMiT <theamit26@gmail.com>
2018-10-23 22:42:33 +0530

    Add files via upload

---
+++ all_paths.py
@@ -0,0 +1,29 @@
+# Python code to find number of unique paths in a  matrix with obstacles.
+
+
+def getPaths(GRID):
+    n = len(GRID)
+    m = len(GRID[0])
+    dp = [[0 for _ in range(m)] for _ in range(n)]
+
+    # initialize first row
+    for i in range(m):
+        if GRID[0][i] == 1:
+            dp[0][i] = 1
+
+    # initialize first column
+    for i in range(n):
+        if GRID[i][0] == 1:
+            dp[i][0] = 1
+    for i in range(1, n):       # row
+        if GRID[i][0] == 1:
+            dp[i][0] = dp[i-1][0]
+        for j in range(1, m):  # column
+            if GRID[i][j] == 1:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    # print(dp)
+    return dp[-1][-1]
+
+
+A = [[1, 1, 1,1], [1, 1, 1,1], [1, 1, 1,1]]
+print(getPaths(A))
